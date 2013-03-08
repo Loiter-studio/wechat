5524f575e5cd7bba9c9fb495b0254d923a7211d2
lovedayluk <lovedayluk@gmail.com>
2013-03-09 01:01:10 +0800

    modify: image download

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ var wechat = require('wechat'),
 		path = require('path'),
 		$ = require('jquery'),
 		S = require('string'),
+		url = require('url'),
 		express = require('express'),
+		fs = require('fs'),
+    request = require('request'),	
 		//connect = require('connect'),
 		fetch = require('./books');
 /*
@@ -36,6 +39,50 @@ app.post("/wechat", wechat('iLibrary', wechat.text(function (message, req, res, 
 		fetch("book", res);
 	})
 ));
+app.get('/:url', function(req, res){
+	//console.log(req.params.image_url);
+	var pathname=__dirname + "/thumbs/" + url.parse(req.params.url).pathname;
+	console.log(pathname);
+	if (path.extname(pathname)=="") {
+		pathname+="/";
+	}
+	if (pathname.charAt(pathname.length-1)=="/"){
+		pathname+="index.html";
+	}
+
+	fs.exists(pathname, function(exists){
+		if(exists){
+			switch(path.extname(pathname)){
+				case ".html":
+					res.writeHead(200, {"Content-Type": "text/html"});
+					break;
+				case ".js":
+					res.writeHead(200, {"Content-Type": "text/javascript"});
+					break;
+				case ".css":
+					res.writeHead(200, {"Content-Type": "text/css"});
+					break;
+				case ".gif":
+					res.writeHead(200, {"Content-Type": "image/gif"});
+					break;
+				case ".jpg":
+					res.writeHead(200, {"Content-Type": "image/jpeg"});
+					break;
+				case ".png":
+					res.writeHead(200, {"Content-Type": "image/png"});
+					break;
+				default:
+					res.writeHead(200, {"Content-Type": "application/octet-stream"});
+			}
+			fs.readFile(pathname,function (err,data){
+				res.end(data);
+			});
+		} else {
+			res.writeHead(404, {"Content-Type": "text/html"});
+			res.end("<h1>404 Not Found</h1>");
+		}
+	});
+});
 
 http.createServer(app).listen(app.get('port'), function(){
 	console.log("listen 3000");
