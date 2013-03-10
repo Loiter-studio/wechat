72f323d6a88a660f3eec469ec05d00f827b13968
lovedayluk <lovedayluk@gmail.com>
2013-03-10 10:16:00 +0800

    modify: use jquery contains

--- books.js
+++ books.js
@@ -60,9 +60,8 @@ var wechat = {
 						};
 						
 						var itemtitle = $(_this).find(".itemtitle");
-						//console.log(itemtitle);
 						var title = $.trim(itemtitle.text());
-						image.url = S($(_this).find("img").attr("src")).trim().s;
+						image.url = $.trim($(_this).find("img").attr("src"));
 						var image_name = image.save();
 						var picurl = "http://lib.sysujwxt.com/thumbs/" + image_name;
 						
@@ -70,10 +69,9 @@ var wechat = {
 						url = url.substring(86, url.length-11).split("&");
 						var set_number = url[1].split("=")[1];
 						var set_entry = url[2].split("=")[1];
-						
 						url = "http://lib.sysujwxt.com/detail/" + "empty" + "-" + image_name + "-" + set_number + "-" + set_entry;
-						var pin = '';
 						
+						var pin = '';
 						var str = $(this).find('table').text().split('\n');
 						if(str.length >= 6){
 							// All operation concern to array, should validate
@@ -92,7 +90,6 @@ var wechat = {
 		});
 	},
 	detail: function(url, res){
-		console.log(url);
 		var _res = res;
 		var _this = this;
 		var book = {};
@@ -117,40 +114,21 @@ var wechat = {
 			}).on('end', function() {
 				var td = $(html).find("div#details2 td");
 				var size = td.length;
-				if(size > 0){
-					var isbn_price = $(td[3]).children("a").text().split(":");
+				if(size > 0){	
+					var isbn_price = $(html).find("td.td1:contains(ISBN)").next().text().split(":");
 					book.isbn = $.trim(isbn_price[0]);
 					book.price = $.trim(isbn_price[1]);
-					var author_publish = $.trim($(td[7]).children("a").text());
-					book.publish = $.trim($($(html).find("td.td1:contains(出版)")[1]).text());	
-					book.author = "";
-					if(author_publish){
-						author_publish = author_publish.split("/");
-						book.title = author_publish[0];
-						book.author = author_publish[1];
-					}
-					book.summary = "";
-					if( $.trim($(td[12]).text()) == "摘要")
-						book.summary = $.trim($(td[13]).text());
-					else if( $.trim($(td[14]).text()) == "摘要")
-						book.summary = $.trim($(td[15]).text());
-					else if( $.trim($(td[16]).text()) == "摘要")
-						book.summary = $.trim($(td[17]).text());
-					else if( $.trim($(td[18]).text()) == "摘要")
-						book.summary = $.trim($(td[19]).text());
+					var title = $(html).find("td.td1:contains(题名)").next().text();
+					title = title.split("/");
+					book.title = $.trim(title[0]);
+					book.author = $.trim(title[1].split("\n")[0]);
+					book.summary = $.trim($(html).find("td.td1:contains(摘要)").next().text());
+					book.pin = $.trim($(html).find("td.td1:contains(索书号)").next().text().split("\n")[1].split(":")[1]);
 					
-					if($.trim($(td[size-6]).text()) == "馆藏地:索书号")
-						book.pin = $.trim($(td[size-5]).text().split(":")[1]);
-					// Get book status
-					var status_url = $(td[size-3]).children("a").attr("href").split("?")[1];
-					var status_options = {  
-							host: '202.116.64.108',  
-							port: 8991,
-							path: '/F?' + status_url ,
-					};
+					var status_url = $(html).find("td.td1:contains(所有单册借阅状态) a").attr("href");
 					var status_html = '';
 					var status = Array();
-					http.get(status_options, function(res) {  
+					http.get(status_url, function(res) {  
 						res.on('data', function(data) {  
 							status_html += data;
 						}).on('end', function() {
@@ -158,8 +136,6 @@ var wechat = {
 							var tmp_counter = 1;
 							tmp_status.each(function(){
 								var single = {};
-								$(this).children("td");
-								
 								single.type = $($(this).children("td")[1]).text();
 								single.status = $($(this).children("td")[2]).text();
 								single.rt_date = $($(this).children("td")[3]).text()
@@ -170,14 +146,13 @@ var wechat = {
 									// status title pin image_name price publish summary 
 									book.code = "success";
 									book.status = status;
-									//console.log(book);
-									_res.end(JSON.stringify(book));
+									console.log(book);
+									//_res.end(JSON.stringify(book));
 								}
 								//_res.end(status_html);
 							});
 						});
 					});	
-					//_res.end(html);
 				}
 				else{
 					book.code = "failed";
@@ -188,4 +163,5 @@ var wechat = {
 	}
 }
 module.exports = wechat;
+//wechat.fetch('苏轼', '');103202&set_entry=000001&
 //wechat.detail("Python%E5%9F%BA%E7%A1%80%E6%95%99%E7%A8%8B-746e42358778e8a6ef93656132d7439e.jpg-101487-000010?sukey=b50423ad109ac255073790da7e9b5fa158df1fcdda963f3a241b45f044aadd34e3dfec297aff5f2a60cb283d6f4a74ca15894aac0d37ab68", "");
